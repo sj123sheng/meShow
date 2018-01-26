112ff6f7b9ffb575816ad87a1cf239f35eed0a4b
shengjian <531522023@qq.com>
2018-01-26 15:15:27 +0800

    fix

--- src/main/java/com/melot/kktv/action/ActorFunction.java
+++ src/main/java/com/melot/kktv/action/ActorFunction.java
@@ -546,7 +546,7 @@ public class ActorFunction {
                 int verifyUserId = userVerifyDO.getUserId();
                 UserApplyActorDO userApplyActorDO = userApplyActorService.getUserApplyActorDO(verifyUserId).getData();
                 //巡管审核驳回 或 家族驳回
-                if (userApplyActorDO.getStatus() < 0 || userApplyActorDO.getStatus() == 6) {
+                if (userApplyActorDO == null || userApplyActorDO.getStatus() < 0 || userApplyActorDO.getStatus() == 6) {
                     continue;
                 }
                 if (verifyUserId == userId) {
