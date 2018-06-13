a5428f95fc0dfa43d54613753d97b1888b1cdf16
anwen.wei@melot.cn <anwen.wei@melot.cn>
2018-06-13 15:10:36 +0800

    去掉不必要的代码

--- src/main/java/com/melot/kkcx/functions/UserLevelFunctions.java
+++ src/main/java/com/melot/kkcx/functions/UserLevelFunctions.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
