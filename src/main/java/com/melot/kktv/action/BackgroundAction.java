5929858f803ff63f0a12ea8d87d82b860c116ffe
林端端 <duanduan.lin@melot.cn>
2018-07-10 12:30:52 +0800

    fix

--- src/main/java/com/melot/kktv/action/BackgroundAction.java
+++ src/main/java/com/melot/kktv/action/BackgroundAction.java
@@ -1,17 +1,21 @@
 package com.melot.kktv.action;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.melot.kk.module.report.service.FeedbackInfoService;
-import com.melot.kktv.util.*;
-import com.melot.sdk.core.util.MelotBeanFactory;
-import org.apache.log4j.Logger;
-
-import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.melot.kk.module.report.service.FeedbackInfoService;
+import com.melot.kktv.redis.AppStatsSource;
+import com.melot.kktv.util.AppChannelEnum;
+import com.melot.kktv.util.AppIdEnum;
+import com.melot.kktv.util.CommonUtil;
+import com.melot.kktv.util.HadoopLogger;
+import com.melot.kktv.util.TagCodeEnum;
+import com.melot.sdk.core.util.MelotBeanFactory;
 
 
 /**
@@ -20,10 +24,7 @@ import java.util.Map;
  *
  */
 public class BackgroundAction {
-
-	/** 日志记录对象 */
-	private static Logger logger = Logger.getLogger(BackgroundAction.class);
-	
+    
 	/**
 	 * 用户反馈接口(10007001)
 	 * 
