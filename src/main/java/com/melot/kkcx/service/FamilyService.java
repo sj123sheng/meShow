5cce21b61fe3f9db81c378b16bbc5457647e4d3f
王志昊 <zhihao.wang@melot.cn>
2018-07-11 14:13:54 +0800

    fix sonar 阻断

--- src/main/java/com/melot/kkcx/service/FamilyService.java
+++ src/main/java/com/melot/kkcx/service/FamilyService.java
@@ -54,7 +54,7 @@ import com.melot.sdk.core.util.MelotBeanFactory;
 public class FamilyService {
 	
 	private static Logger logger = Logger.getLogger(FamilyService.class);
-	
+
 	/**
 	 * 获取分类下家族总数
 	 * @param basicDBObject
@@ -221,12 +221,14 @@ public class FamilyService {
 						familyPosterJson = new JsonObject();
 						familyPosterJson.addProperty("path_174", familyPoster.getPath_174());
 					}
+					break;
 				case PlatformEnum.IPHONE:
 					// 返回 222*148px
 					if (familyPoster.getPath_original() != null) {
 						familyPosterJson = new JsonObject();
 						familyPosterJson.addProperty("path_222", familyPoster.getPath_222());
 					}
+					break;
 				case PlatformEnum.IPAD:
 					// 返回 222*148px
 					if (familyPoster.getPath_original() != null) {
@@ -268,12 +270,14 @@ public class FamilyService {
 						familyPosterJson = new JsonObject();
 						familyPosterJson.addProperty("path_174", familyPoster.getPath_174());
 					}
+					break;
 				case PlatformEnum.IPHONE:
 					// 返回 222*148px
 					if (familyPoster.getPath_original() != null) {
 						familyPosterJson = new JsonObject();
 						familyPosterJson.addProperty("path_222", familyPoster.getPath_222());
 					}
+					break;
 				case PlatformEnum.IPAD:
 					// 返回 222*148px
 					if (familyPoster.getPath_original() != null) {
