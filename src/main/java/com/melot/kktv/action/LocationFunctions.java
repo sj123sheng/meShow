da7870d6693699b31422b9542a909e28c3c95f37
shengjian <531522023@qq.com>
2018-08-24 09:30:38 +0800

    fix

--- src/main/java/com/melot/kktv/action/LocationFunctions.java
+++ src/main/java/com/melot/kktv/action/LocationFunctions.java
@@ -63,17 +63,20 @@ public class LocationFunctions {
             if(addressComponentDTO != null && StringUtils.isNotEmpty(addressComponentDTO.getTown())
                     && StringUtils.isNotEmpty(addressComponentDTO.getTownAreaCode())){
 
-                String d =
-                result.addProperty("province", addressComponentDTO.getProvince());
-                result.addProperty("city", addressComponentDTO.getCity());
-                result.addProperty("district", addressComponentDTO.getDistrict());
-                result.addProperty("town", addressComponentDTO.getTown());
-                result.addProperty("townAreaCode", addressComponentDTO.getTownAreaCode());
-                if(userId > 0 && isOwner && StringUtils.isNotEmpty(addressComponentDTO.getTownAreaCode())) {
-                    TownUserInfoParam userInfoParam = new TownUserInfoParam();
-                    userInfoParam.setUserId(userId);
-                    userInfoParam.setLastAreaCode(addressComponentDTO.getTownAreaCode());
-                    townUserService.saveUserInfo(userInfoParam);
+                String townAreaCode = addressComponentDTO.getTownAreaCode();
+
+                if(StringUtils.isNotEmpty(townAreaCode)) {
+                    result.addProperty("province", addressComponentDTO.getProvince());
+                    result.addProperty("city", addressComponentDTO.getCity());
+                    result.addProperty("district", addressComponentDTO.getDistrict());
+                    result.addProperty("town", addressComponentDTO.getTown());
+                    result.addProperty("townAreaCode", townAreaCode);
+                    if (userId > 0 && isOwner) {
+                        TownUserInfoParam userInfoParam = new TownUserInfoParam();
+                        userInfoParam.setUserId(userId);
+                        userInfoParam.setLastAreaCode(addressComponentDTO.getTownAreaCode());
+                        townUserService.saveUserInfo(userInfoParam);
+                    }
                 }
             }
             result.addProperty("TagCode", TagCodeEnum.SUCCESS);
