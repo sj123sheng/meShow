48ef80c57bcf058abc11f54678be7279d02576b7
anwen.wei@melot.cn <anwen.wei@melot.cn>
2018-08-29 20:20:00 +0800

    fix bug

--- src/main/java/com/melot/kktv/action/HallFunctions.java
+++ src/main/java/com/melot/kktv/action/HallFunctions.java
@@ -300,7 +300,9 @@ public class HallFunctions {
 				while (i < roomList.size()) {
 					roomInfo = roomList.get(i++);
 					json = HallRoomTF.roomInfoWithPlaybackToJson(roomInfo, platform);
-					if (sysMenu.getDataSourceType() != null && sysMenu.getDataSourceType() == 16) {
+					if (sysMenu.getDataSourceType() != null 
+					        && (sysMenu.getDataSourceType() == 16
+					        || sysMenu.getDataSourceType() == 17)) {
 						json.addProperty("distance", 0);
 					}
 					roomArray.add(json);
