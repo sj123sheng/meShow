bbf0b3936f68545791348266aa0957f80f7e9552
林端端 <duanduan.lin@melot.cn>
2018-09-11 14:23:31 +0800

    获取用户粉丝排行榜(10003013) 周榜及月榜缓存时间改为1小时 获得自定义财富等级列表（51010904）新增 记录不同渠道推广记录（51120301）调整 获取accessToken（51120302）新增 上传用户的formId（51120303）新增 上传用户的信息（51120304）新增

--- src/main/java/com/melot/kkcx/functions/WechatProjectFunctions.java
+++ src/main/java/com/melot/kkcx/functions/WechatProjectFunctions.java
@@ -1,16 +1,25 @@
 package com.melot.kkcx.functions;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import com.melot.kk.wechatProject.api.Service.WechatPromoteService;
+import com.google.gson.reflect.TypeToken;
+import com.melot.kk.wechatProject.api.Service.WechatCommonService;
+import com.melot.kktv.service.ConfigService;
 import com.melot.kktv.service.GeneralService;
 import com.melot.kktv.util.CommonUtil;
 import com.melot.kktv.util.ParameterKeys;
+import com.melot.kktv.util.SecurityFunctions;
 import com.melot.kktv.util.TagCodeEnum;
+import com.melot.kktv.util.cache.EhCache;
 import com.melot.module.api.exceptions.MelotModuleException;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.Map;
+
 
 /**
  * Title: WechatProjectFunctions
@@ -26,8 +35,11 @@ public class WechatProjectFunctions {
 
     private static Logger log = Logger.getLogger(WechatProjectFunctions.class);
 
+    @Autowired
+    private ConfigService configService;
+
     @Resource
-    private WechatPromoteService wechatPromoteService;
+    private WechatCommonService wechatCommonService;
 
     /**
      * 51120301
@@ -53,7 +65,7 @@ public class WechatProjectFunctions {
         String tagCode = TagCodeEnum.MODULE_UNKNOWN_RESPCODE;
         try {
             ipAddr = GeneralService.getIpAddr(request, 1, 1, null);
-            wechatPromoteService.addPromoteHistByChannel(ipAddr, channel, appId);
+            wechatCommonService.addPromoteHistByChannel(ipAddr, channel, appId);
             tagCode = TagCodeEnum.SUCCESS;
         } catch(MelotModuleException e) {
             log.info(String.format("addPromoteHistByChannel fail: ipAddr=%s, channel=%s, addId=%s", ipAddr, channel, appId), e);
@@ -62,6 +74,120 @@ public class WechatProjectFunctions {
             }
         } catch (Exception e) {
             log.error(String.format("addPromoteHistByChannel error: ipAddr=%s, channel=%s, addId=%s)", ipAddr, channel, appId), e);
+        }
+        result.addProperty(ParameterKeys.TAG_CODE, tagCode);
+        return result;
+    }
+
+    /**
+     * 51120302
+     * 获取accessToken
+     */
+    public JsonObject getAccessToken(JsonObject jsonObject, boolean checkTag, HttpServletRequest request) {
+        JsonObject result = new JsonObject();
+
+        String appId;
+        String appSecret;
+        try {
+            appId = CommonUtil.getJsonParamString(jsonObject, "appId", null, TagCodeEnum.PARAMETER_PARSE_ERROR, 1, Integer.MAX_VALUE);
+            appSecret = CommonUtil.getJsonParamString(jsonObject, "appSecret", null, TagCodeEnum.PARAMETER_PARSE_ERROR, 1, Integer.MAX_VALUE);
+        } catch (CommonUtil.ErrorGetParameterException e) {
+            result.addProperty(ParameterKeys.TAG_CODE, e.getErrCode());
+            return result;
+        } catch (Exception e) {
+            result.addProperty(ParameterKeys.TAG_CODE, TagCodeEnum.PARAMETER_PARSE_ERROR);
+            return result;
+        }
+
+        String tagCode = TagCodeEnum.MODULE_UNKNOWN_RESPCODE;
+        try {
+            String accessToken = wechatCommonService.getAccessToken(appId, appSecret);
+            if (accessToken != null) {
+                result.addProperty("accessToken", accessToken);
+                tagCode = TagCodeEnum.SUCCESS;
+            }
+        } catch (Exception e) {
+            log.error(String.format("getAccessToken error: appId=%s, appSecret=%s)", appId, appSecret), e);
+        }
+        result.addProperty(ParameterKeys.TAG_CODE, tagCode);
+        return result;
+    }
+
+    /**
+     * 51120303
+     * 上传用户的formId
+     */
+    public JsonObject uploadFormId(JsonObject jsonObject, boolean checkTag, HttpServletRequest request) {
+        JsonObject result = new JsonObject();
+
+        String openId;
+        String formId;
+        try {
+            openId = CommonUtil.getJsonParamString(jsonObject, "openId", null, TagCodeEnum.PARAMETER_PARSE_ERROR, 1, Integer.MAX_VALUE);
+            formId = CommonUtil.getJsonParamString(jsonObject, "formId", null, TagCodeEnum.PARAMETER_PARSE_ERROR, 1, Integer.MAX_VALUE);
+        } catch (CommonUtil.ErrorGetParameterException e) {
+            result.addProperty(ParameterKeys.TAG_CODE, e.getErrCode());
+            return result;
+        } catch (Exception e) {
+            result.addProperty(ParameterKeys.TAG_CODE, TagCodeEnum.PARAMETER_PARSE_ERROR);
+            return result;
+        }
+
+        String tagCode = TagCodeEnum.MODULE_UNKNOWN_RESPCODE;
+        try {
+            if (wechatCommonService.addFormId(openId, formId)) {
+                tagCode = TagCodeEnum.SUCCESS;
+            } else {
+                tagCode = "5112030301";
+            }
+        } catch (Exception e) {
+            log.error(String.format("uploadFormId error: openId=%s, formId=%s)", openId, formId), e);
+        }
+        result.addProperty(ParameterKeys.TAG_CODE, tagCode);
+        return result;
+    }
+
+    /**
+     * 51120304
+     * 上传用户信息
+     */
+    public JsonObject uploadUserInfo(JsonObject jsonObject, boolean checkTag, HttpServletRequest request) {
+        JsonObject result = new JsonObject();
+
+        if (!checkTag) {
+            result.addProperty(ParameterKeys.TAG_CODE, TagCodeEnum.TOKEN_INCORRECT);
+            return result;
+        }
+
+        int userId;
+        String appType;
+        String openId;
+        try {
+            userId = CommonUtil.getJsonParamInt(jsonObject, "userId", 0, TagCodeEnum.INVALID_PARAMETERS, 1, Integer.MAX_VALUE);
+            appType = CommonUtil.getJsonParamString(jsonObject, "appType", null, TagCodeEnum.INVALID_PARAMETERS, 1, 512);
+            openId = CommonUtil.getJsonParamString(jsonObject, "openId", null, TagCodeEnum.INVALID_PARAMETERS, 1, 512);
+        } catch (CommonUtil.ErrorGetParameterException e) {
+            result.addProperty(ParameterKeys.TAG_CODE, e.getErrCode());
+            return result;
+        } catch (Exception e) {
+            result.addProperty(ParameterKeys.TAG_CODE, TagCodeEnum.INVALID_PARAMETERS);
+            return result;
+        }
+
+
+        String tagCode = TagCodeEnum.MODULE_UNKNOWN_RESPCODE;
+        try {
+            Map<String, String> appIds = new Gson().fromJson(configService.getWechatProjectAppIds(), new TypeToken<java.util.Map<String, String>>() {}.getType());
+            String appId = appIds.get(appType);
+            if (appId == null) {
+                result.addProperty(ParameterKeys.TAG_CODE, TagCodeEnum.INVALID_PARAMETERS);
+                return result;
+            }
+            if (EhCache.getFromCache(String.format("wx_openId_%s_%s", userId, appId)) == null) {
+                tagCode = wechatCommonService.addUserInfo(userId, openId, appId) ? TagCodeEnum.SUCCESS : "5112030401";
+            }
+        } catch (Exception e) {
+            log.error(String.format("uploadFormId error: userId=%s, appType=%s, openId=%s)", userId, appType, openId), e);
         }
         result.addProperty(ParameterKeys.TAG_CODE, tagCode);
         return result;
